1baf5952f1278b943312535447a49e9f71eb1d98
ShahnozSaidzod <shahnozasaidova1989@gmail.com>
2023-11-01 16:27:57 -0400

    delete print statement

--- src/Test/Login.java
+++ src/Test/Login.java
@@ -2,9 +2,7 @@ package Test;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println("Login to application");
 
-        System.out.println("Hello world");
-        System.out.println("Im practising some git and github");
+
     }
 }
