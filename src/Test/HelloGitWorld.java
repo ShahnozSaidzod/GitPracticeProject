7a05dda43b9a2d9fc1e5835c82de8469656dd01b
ShahnozSaidzod <shahnozasaidova1989@gmail.com>
2023-11-01 14:44:46 -0400

    Made some changes

--- src/Test/HelloGitWorld.java
+++ src/Test/HelloGitWorld.java
@@ -3,5 +3,6 @@ package Test;
 public class HelloGitWorld {
     public static void main(String[] args) {
         System.out.println("Hello Git World");
+        System.out.println("Hello Git world 2");
     }
 }
